5baa68945e83c77a8524abb5f1bedfd9fc7b5b15
ZC <83486674+zc360@users.noreply.github.com>
2024-09-14 17:55:13 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -31,17 +31,7 @@ export default defineUserConfig({
   shouldPrefetch: true,
   // 主题配置 FileList 是 vuepress 的一个主题，文件展示的功能全部由这个主题提供。
   theme: FileList([
-    {
-      // 挂载路径
-      mountPath: "/",
-      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
-      analysis: githubReleasesFilesAnalysis({
-        // 仓库所有者的用户名
-        user: "zc360",
-        // 仓库所有者的仓库名
-        repository: "Drive"
-      }),
-    },
+
     {
       mountPath: "/",
       analysis: githubReleasesFilesAnalysis({ user: "zc360", repository: "Drive" }),
